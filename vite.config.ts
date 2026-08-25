4d7bd8ce2b0052d0e860326e1c68909163df24d1
Vigan Kuka <kukavigan@gmail.com>
2026-08-25 15:19:13 +0200

    Configure Github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lola-restaurant/',
   plugins: [react()],
   resolve: {
     alias: {
